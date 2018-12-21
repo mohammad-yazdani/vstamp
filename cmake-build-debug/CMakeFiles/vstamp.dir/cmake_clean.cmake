8e8bc0647a05c38cda00e03803227bc0a01bc7ec
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2018-12-21 12:55:00 -0500

    init

---
+++ cmake-build-debug/CMakeFiles/vstamp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vstamp.dir/main.cpp.o"
+  "vstamp.pdb"
+  "vstamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vstamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/vstamp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vstamp.
+# This may be replaced when dependencies are built.
